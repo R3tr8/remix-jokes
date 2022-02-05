1b49a4bb56e9e12c8e26c8eca1a585aa029334e9
R3tr8 <franckemmanuel.jeanmarie@gmail.com>
2022-02-05 00:10:08 -0400

    premier commit

---
+++ app/routes/jokes/$jokeId.tsx
@@ -0,0 +1,10 @@
+export default function JokesRoute() {
+    return(
+        <div>
+            <p>Here's your joke:</p>
+            <p>
+                Ils ont prit de la drogue ou quoi les devs !!
+            </p>
+        </div>
+    );
+}
---
+++ app/routes/jokes/index.tsx
@@ -0,0 +1,11 @@
+export default function JokesRoute() {
+    return(
+        <div>
+            <p>Here's a random joke:</p>
+            <p>
+                Qu'est ce qui  n'est pas un steack ? ...
+                ah bah une pute 🤪🤪🤪
+            </p>
+        </div>
+    )
+}
