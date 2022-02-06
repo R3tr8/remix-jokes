57131d0c885b57e3d69f13c51dca42bc40f9484f
R3tr8 <franckemmanuel.jeanmarie@gmail.com>
2022-02-05 20:34:24 -0400

    affichage aléatoire d'une blague + classement des blagues selon leur date de création + possibilité de créer une nouvelle blague et de l'enregistrer dans la db

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,10 +1,29 @@
-export default function JokesRoute() {
-    return(
-        <div>
-            <p>Here's your joke:</p>
-            <p>
-                Ils ont prit de la drogue ou quoi les devs !!
-            </p>
-        </div>
-    );
+import type { LoaderFunction } from "remix";
+import { Link, useLoaderData } from "remix";
+import type { Joke } from "@prisma/client";
+import { db } from "~/utils/db.server";
+
+type LoaderData = { joke: Joke };
+
+export const loader: LoaderFunction = async ({
+  params
+}) => {
+  const joke = await db.joke.findUnique({
+    where: { id: params.jokeId }
+  });
+  if (!joke) throw new Error("Joke not found");
+  const data: LoaderData = { joke };
+  return data;
+};
+
+export default function JokeRoute() {
+  const data = useLoaderData<LoaderData>();
+
+  return (
+    <div>
+      <p>Here's your hilarious joke:</p>
+      <p>{data.joke.content}</p>
+      <Link to=".">{data.joke.name} Permalink</Link>
+    </div>
+  );
 }
--- app/routes/jokes/index.tsx
+++ app/routes/jokes/index.tsx
@@ -1,11 +1,31 @@
-export default function JokesRoute() {
-    return(
-        <div>
-            <p>Here's a random joke:</p>
-            <p>
-                Qu'est ce qui  n'est pas un steack ? ...
-                ah bah une pute 🤪🤪🤪
-            </p>
-        </div>
-    )
+import type { LoaderFunction } from "remix";
+import { useLoaderData, Link } from "remix";
+import type { Joke } from "@prisma/client";
+import { db } from "~/utils/db.server";
+
+type LoaderData = { randomJoke: Joke };
+
+export const loader: LoaderFunction = async () => {
+  const count = await db.joke.count();
+  const randomRowNumber = Math.floor(Math.random() * count);
+  const [randomJoke] = await db.joke.findMany({
+    take: 1,
+    skip: randomRowNumber
+  });
+  const data: LoaderData = { randomJoke };
+  return data;
+};
+
+export default function JokesIndexRoute() {
+  const data = useLoaderData<LoaderData>();
+
+  return (
+    <div>
+      <p>Here's a random joke:</p>
+      <p>{data.randomJoke.content}</p>
+      <Link to={data.randomJoke.id}>
+        "{data.randomJoke.name}" Permalink
+      </Link>
+    </div>
+  );
 }
